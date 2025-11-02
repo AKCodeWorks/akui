a2c0a79a8b6ca372279f4398941623da4ed58b04
Adam Gillispie <95198544+agillispie@users.noreply.github.com>
2025-11-01 23:49:50 -0400

    delete unecessary published files

--- src/lib/components/akui/accordion/accordion-dep.svelte
+++
@@ -1,6 +0,0 @@
-<!-- AKUI_VERSION: 0.00 DO NOT DELETE OR YOUR FILE WILL BE OVERWRITTEN ON UPDATE! -->
-<script lang="ts">
-	let someDep = $state('I am an accordion');
-</script>
-
-<h2>{someDep}</h2>
